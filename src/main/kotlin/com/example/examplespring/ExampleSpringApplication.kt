c6808d5bb93b63c5356adde1880aecc4c35a6be6
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-09-02 14:05:17 +0700

    Initial commit

---
+++ src/main/kotlin/com/example/examplespring/ExampleSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.examplespring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExampleSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExampleSpringApplication>(*args)
+}
